0f09d54c68124c9035a0b693bcce564878ea24b2
Andrew Nesbitt <andrewnez@gmail.com>
2013-06-11 16:27:29 +0100

    Actual functioning task

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,21 +30,8 @@ module.exports = function(grunt) {
 
     // Configuration to be run (and then tested).
     nodecopter: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
+      options: {
+        ip: '192.168.1.1'
       },
     },
 
